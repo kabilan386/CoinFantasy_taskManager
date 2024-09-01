20254b2c4d2b69b3184a86212aea792a40e7563f
kabilan386 <kabilankabi386@gmail.com>
2024-09-01 23:35:13 +0530

    update live mysql and backend url

--- task-manager/server/src/config/database.js
+++ task-manager/server/src/config/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('task_manage', 'root', 'password', {
-  host: 'mysql',
+const sequelize = new Sequelize('bparcum4vaubczrh7wcm', 'u9zc6gse2on73nrq', '5trqpukHjhkjRiouuCR0', {
+  host: 'bparcum4vaubczrh7wcm-mysql.services.clever-cloud.com',
   dialect: 'mysql',
   port: 3306 // Optional, default is 3306
 });
